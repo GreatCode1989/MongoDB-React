2d4fd3be367c200fb852f53078db1ecbd1577187
GreatCode1989 <wef666wef6@gmail.com>
2023-10-03 18:57:06 +0300

    lo

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator"
-
 import { registerValidation } from "./validation/auth.js";
-
-import UserModel from "./models/user.js"
+import checkAuth from "./utils/checkAuth.js";
+import * as userController from './controllers/userController.js'
 
 mongoose
   .connect(
-    "mongodb+srv://wef353wef535:Devil666devil3@cluster0.o3z7u6s.mongodb.net/"
+    "mongodb+srv://wef353wef535:Devil666devil3@cluster0.o3z7u6s.mongodb.net/blog"
   )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
@@ -18,23 +15,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-    }
+app.post("/auth/login", userController.register);
 
-const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.avatarUrl
-})
+app.post("/auth/register", registerValidation, userController.login);
 
-    res.json({
-        success: true
-    })
-});
+app.get("/auth/me", checkAuth, userController.getMe);
 
 app.listen(3005, (err) => {
   if (err) {
